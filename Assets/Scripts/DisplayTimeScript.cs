3f12c384308960b29fe6b6bbdc2dd453847f5ca9
Hunter Williams <hunter@huntergwilliams.com>
2017-04-04 18:26:45 +0700

    most of initial world menu laid out

---
+++ Assets/Scripts/DisplayTimeScript.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.UI.Extensions;
+
+public class DisplayTimeScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    // Update is called once per frame
+    private void FixedUpdate()
+    {
+        this.GetComponent<Text>().text = DateTime.Now.ToString("HH:mm");
+    }
+
+}
---
+++ Assets/Scripts/TogglableActive.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TogglableActive : MonoBehaviour {
+
+    public string cow;
+	public bool ToggleActive()
+    {
+        bool nextState = !this.gameObject.activeSelf;
+        this.gameObject.SetActive(nextState);
+
+        return nextState;
+    }
+}

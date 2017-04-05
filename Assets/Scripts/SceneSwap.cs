57803edb285c78f2353e2ff0ac7ecb94d7311c2d
Hunter Williams <hunter@huntergwilliams.com>
2017-04-05 18:43:07 +0700

    moving between home and menu scenes

---
+++ Assets/Scripts/SceneSwap.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneSwap : MonoBehaviour {
+
+	public void ChangeToWorldScene()
+    {
+        print("loading main");
+        SceneManager.LoadScene("main");
+    }
+
+    public void ChangeToMenuScene()
+    {
+        print("loading menu");
+        SceneManager.LoadScene("customise");
+    }
+}
